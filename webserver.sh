63590b0393dc90cd7e8822989503ac9a72c3e0f8
dukeseb <109305417+dukeseb@users.noreply.github.com>
2025-01-22 11:00:14 -0400

    Update and rename ubuntu.sh to webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/dukeseb/Proxmox/refs/heads/main/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -7,11 +7,11 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 
 # App Default Values
 echo -e "Loading..."
-APP="Ubuntu"
+APP="Webserver"
 var_tags="os"
 var_cpu="1"
 var_ram="512"
-var_disk="2"
+var_disk="8"
 var_os="ubuntu"
 var_version="24.04"
 
